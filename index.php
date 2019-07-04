f2393d5204587cd999df9266826dc2cff1301159
sequencedigitale <info@sequencedigitale.com>
2019-07-04 10:49:08 -0400

    Premier depot

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html class="nojs" lang="fr" dir="ltr" data-form-state="!closed">
+<head>
+
+
+
+<!-- CODE :: Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-87186208-51&api=1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-87186208-51', { 'anonymize_ip': true });
+</script>
+<!-- /CODE :: Google Analytics -->
+
+
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1, user-scalable=no,  maximum-scale=1.0, user-scalable=no">
+	<meta name="robots" content="index,follow">
+	<link rel="icon" type="image/x-icon" href="https://ici.tou.tv/favicon.ico">
+	<script src="https://assets.adobedtm.com/2eda04f28b1fa2bbcd3ec449dfdc174232ed3359/satelliteLib-fafff6f6fa41c8ef8818e2ad8c1bfb4776de2f18.js"></script>
+
+
+
+<!-- CODE :: Meta -->
+<title>@@@1 | ICI Radio-Canada.ca</title>
+<meta name="Description" content="@@@3b" />
+<meta name="Keywords" content="@@@3a" />
+<meta name="dc.title" content="@@@1 | ICI Radio-Canada.ca" />
+<meta name="dc.keywords" content="@@@3a" />
+<meta name="dc.description" content="@@@3b" />
+<meta property="og:url" content="@@@4a" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="@@@1 | ICI Radio-Canada.ca" />
+<meta property="og:description" content="@@@3b" />
+<meta property="og:image" content="@@@4b" />
+<!-- /CODE :: Meta -->
+<!-- CODE :: Omniture -->
+<script>
+dataLayerQueue.push ({
+	page : {
+		Section : "",
+		GroupeSection : "",
+		Segment : "",
+		CodePage : ""
+	},
+	dataPushType : "prePageView"
+});
+</script>
+<!-- /CODE :: Omniture -->
+
+
+
+	<script type="text/javascript"><!--
+	var html = document.getElementsByTagName('html')[0];
+	html.className = html.className.replace('nojs', 'js');
+	--></script>
+
+
+
+<!-- CODE :: Stylesheets -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+<link rel="stylesheet" href="assets/css/toutv.css?<?php echo filemtime('assets/css/toutv.css'); ?>" />
+<link rel="stylesheet" href="assets/css/style.css?<?php echo filemtime('assets/css/style.css'); ?>" />
+<!-- /CODE :: Stylesheets -->
+
+
+
+</head>
+<body>
+
+
+
+<!-- CODE :: Pixel de reciblage -->
+<div id="pixel">
+	<img src="http://lien-vers-le-pixel.jpg" alt="" height="1" width="1" />
+</div>
+<!-- /CODE :: Pixel de reciblage -->
+
+
+
+<div id="app">
+	<div class="layout" id="layout" data-reactroot="">
+<!-- TOU.TV - header -->
+<header id="toutv-header" role="banner"><div class="inner inner-flex">
+	<div class="logo"><a href="https://ici.tou.tv/" target="_blank"><img src="https://ici.tou.tv/images/logo-toutv.svg" alt="ICI Tou.tv" height="41" width="162" /></a></div>
+	<div class="menu">
+		<button aria-hidden="true" id="menu-button" class="menu-button" type="button" aria-haspopup="true" aria-expanded="false" aria-controls="menu-wrapper" aria-label="Navigation"><span><span class="accessibility__msg-hs">Menu</span></span></button>
+		<div id="menu-wrapper" class="menu-wrapper">
+			<ul>
+				<li class=""><a href="https://ici.tou.tv/mes-visionnements">MON TOU.TV</a></li>
+				<li class=""><a href="https://ici.tou.tv/rattrapage">Rattrapage</a></li>
+				<li class=""><a href="https://ici.tou.tv/">Parcourir</a></li>
+				<li class=""><a href="https://ici.tou.tv/categorie/jeunesse">Jeunesse</a></li>
+				<li class=""><a href="https://ici.tou.tv/categorie/extra">EXTRA</a></li>
+			</ul>
+		</div>
+	</div>
+</div></header>
+<!-- /TOU.TV - header -->
+
+
+
+<!-- CODE :: sponsored -->
+<div id="sponsored"><div class="inner">Contenu commandité</div></div>
+<!-- /CODE :: sponsored -->
+
+
+
+		<span tabindex="-1" class="accessibility__msg-hs">Vous êtes dans le contenu principal</span>
+		<main id="main" class="section emisode-page" role="main">
+
+
+
+<!-- CODE :: Debut du Microsite-->
+<div id="wrapper">
+
+
+</div>
+<!-- CODE :: Fin du Microsite-->
+
+
+
+		</main>
+<!-- TOU.TV - footer -->
+<footer id="toutv-footer" role="contentinfo"><div class="inner">
+	<div class="nav-footer">
+		<ul>
+			<li><a href="http://www.cbc.radio-canada.ca/fr/rendre-des-comptes-aux-canadiens/lois-et-politiques/regles-utilisation/">Conditions d&#x27;utilisation<span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="http://assistance.radio-canada.ca">Assistance technique<span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="https://radiocanada.zendesk.com/hc/fr/requests/new">Assistance abonnement<span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="https://ici.tou.tv/nos-plateformes">Nos plateformes<span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="http://ici.radio-canada.ca/accessibilite-web">Accessibilité<span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+		</ul>
+	</div>
+	<div class="socials">
+		<ul>
+			<li><a href="http://facebook.com/icitoutv"><span class="icon icon-facebook">&#xE93F;</span><span class="accessibility__msg-hs"> Facebook</span><span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="http://twitter.com/icitoutv"><span class="icon icon-twitter">&#xE945;</span><span class="accessibility__msg-hs"> Twitter</span><span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+			<li><a href="http://instagram.com/icitoutv"><span class="icon icon-instagram">&#xE940;</span><span class="accessibility__msg-hs"> Instagram</span><span class="accessibility__msg-hs"><!-- -->Ce lien ou bouton renvoie vers une autre page</span></a></li>
+		</ul>
+	</div>
+</div></footer>
+<!-- /TOU.TV - footer -->
+	</div>
+</div>
+
+
+
+<!-- CODE :: Javascripts -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/lightbox-toutv.js?<?php echo filemtime('assets/js/lightbox-toutv.js'); ?>"></script>
+<script type="text/javascript" src="assets/js/share.js?<?php echo filemtime('assets/js/share.js'); ?>"></script>
+<!-- /CODE :: Javascripts -->
+
+
+
+</body>
+</html>
